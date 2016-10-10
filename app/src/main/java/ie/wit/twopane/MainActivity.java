2e5ce223b3ead540bf003b3e42e42a681274156f
michaelfoy <mikkfoy@gmail.com>
2016-10-10 19:59:24 +0100

    up to step 05, not complete

--- app/src/main/java/ie/wit/twopane/MainActivity.java
+++ app/src/main/java/ie/wit/twopane/MainActivity.java
@@ -1,13 +1,29 @@
 package ie.wit.twopane;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+  ActionBar actionBar;
+
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.fragment_2);
+
+    actionBar = getSupportActionBar();
+
+    FragmentManager manager = getSupportFragmentManager();
+    Fragment fragment = manager.findFragmentById(R.id.detailFragmentContainer);
+    if (fragment == null)
+    {
+      fragment = new Fragment_1();
+      manager.beginTransaction().add(R.id.detailFragmentContainer, fragment).commit();
+    }
   }
 }
